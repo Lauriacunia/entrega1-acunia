0ec32c378c426777093630bbbef0764da5e821f2
Lauriacunia <lauriacunia@gmail.com>
2022-10-23 12:59:19 -0300

    Avanzo con maquetado de home. Agrego navbar hero y post-list

---
+++ docs.md
@@ -0,0 +1,3 @@
+#### Dudas y comentarios
+
+- No me funcionó la herencia de templates con {% extends %}. Nunca me mostraba la plantilla hija. La única solución fue usa 'include' en el padre.
--- post/views.py
+++ post/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
-
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2018'
+    }
+]
 def showHome(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'posts': posts})
