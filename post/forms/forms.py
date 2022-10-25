65116e1f0f8224e99df0e89451db0864de0dd1e2
Lauriacunia <lauriacunia@gmail.com>
2022-10-25 10:57:51 -0300

    Avanzo con maquedo de form para crear nuevo post

--- post/forms/forms.py
+++ post/forms/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.forms import TextInput
 
 from ..models import Post
 
@@ -7,3 +8,20 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'content')
+        #agregar clases al label
+        widgets = {
+            'title': TextInput(attrs={'class': 'input is-primary m-5',
+                                          'placeholder': 'e.g. Learn Python'}),
+                                    
+
+                      
+            'content': TextInput(attrs={'class': 'textarea is-primary m-5 is-medium',
+                                        'placeholder': 'type your content here'
+                                        }),
+                                        
+                                    
+                                
+
+        }
+
+       
