125e9b8a74473cc403cb8d385892e7adde13ed34
Lauriacunia <lauriacunia@gmail.com>
2022-11-08 12:46:16 -0300

    Agrego view para registro de usuario

--- user/views.py
+++ user/views.py
@@ -1,14 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views import View
-
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.contrib.auth.models import User
+from .forms.forms import CreateUserForm
 # Create your views here.
 
 class RegisterUser(View):
+      
     def get(self, request):
-        return render(request, 'register.html')
+        form = CreateUserForm()
+        return render(request, 'register.html', {'form': form})
 
     def post(self, request):
-        return render(request, 'register.html')
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+        else:
+            return redirect('register')
+
 
 class LoginUser(View):
     def get(self, request):
